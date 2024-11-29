b6da6f01e807e2c9c02df69125841f7d7ba94f63
Alejandro <alejandronicolasdelalamo@gmail.com>
2024-11-29 11:02:37 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const cardElement = document.getElementById("card");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomCard() {
+  const suits = ["♥", "♦", "♣", "♠"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const number = numbers[Math.floor(Math.random() * numbers.length)];
+
+  return { suit, number };
+}
+
+function updateCard() {
+  const { suit, number } = getRandomCard();
+
+  cardElement.querySelector(".top-suit").textContent = suit;
+  cardElement.querySelector(".card-number").textContent = number;
+  cardElement.querySelector(".bottom-suit").textContent = suit;
+
+  const isRed = suit === "♥" || suit === "♦";
+  cardElement.style.color = isRed ? "red" : "black";
+}
+updateCard();
